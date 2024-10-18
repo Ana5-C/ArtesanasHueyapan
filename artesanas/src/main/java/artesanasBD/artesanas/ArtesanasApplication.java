68dc3481ca56d06bf05676e1e891ffe642809eaf
Ana Cristina Martinez <am8794041@gmail.com>
2024-10-18 12:12:17 -0600

    commit Carpeta con dependencias
    se agregaron carpeta del proyecto junto las dependecias

---
+++ artesanas/src/main/java/artesanasBD/artesanas/ArtesanasApplication.java
@@ -0,0 +1,13 @@
+package artesanasBD.artesanas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtesanasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtesanasApplication.class, args);
+	}
+
+}
