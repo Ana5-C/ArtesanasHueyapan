e20788fba459dbe45e731e5761033ae10e57c4a8
Ana Cristina Martinez <am8794041@gmail.com>
2024-11-28 15:07:38 -0600

    Eliminar
    se elimino la carpeta incompleta

--- artesanas/src/main/java/artesanasBD/artesanas/repository/CartProductRepository.java
+++
@@ -1,9 +0,0 @@
-// package artesanasBD.artesanas.repository;
-
-// import org.springframework.data.jpa.repository.JpaRepository;
-
-// import artesanasBD.artesanas.model.CartProduct;
-
-// public interface CartProductRepository extends JpaRepository<CartProduct, Long>{
-
-// }
--- artesanas/src/main/java/artesanasBD/artesanas/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package artesanasBD.artesanas.repository;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import artesanasBD.artesanas.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long>{
-   // List<Product> findByName(String name);
-}
